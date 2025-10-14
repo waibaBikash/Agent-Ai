1c1d4ceee5a84abc1b580947fe551da7fafcddca
waibaBikash <kavrelibikash@gmail.com>
2025-10-14 21:18:54 +1100

    fix: correct formatting and add missing semicolons in ThemeToggleBtn component

--- src/components/ThemeToggleBtn.jsx
+++ src/components/ThemeToggleBtn.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react'
-import assets from '../assets/assets'
+import { useEffect } from 'react';
+import assets from '../assets/assets';
 
 const ThemeToggleBtn = ({theme, setTheme}) => {
 
@@ -23,13 +23,21 @@ const ThemeToggleBtn = ({theme, setTheme}) => {
     <>
        <button>
          {theme === 'dark' ? (
-           <img onClick={()=> setTheme('light')} src={assets.sun_icon} className='size-8.5 p-1.5 border border-gray-500 rounded-full'  alt="" />
+           <img 
+            onClick={()=> setTheme('light')} 
+            src={assets.sun_icon} 
+            className='size-8.5 p-1.5 border border-gray-500 rounded-full'
+              alt="" />
          ) : (
-           <img onClick={()=> setTheme('dark')} src={assets.moon_icon} className='ize-8.5 p-1.5 border border-gray-500 rounded-full' alt/>
+           <img 
+           onClick={()=> setTheme('dark')}
+            src={assets.moon_icon}
+             className='ize-8.5 p-1.5 border border-gray-500 rounded-full'
+             alt/>
          )}
        </button>
     </>
   )
 }
 
-export default ThemeToggleBtn
+export default ThemeToggleBtn;

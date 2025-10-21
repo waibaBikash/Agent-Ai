c5629e2aaff8df7dc4bfa6c8bbe248a5cce8d654
waibaBikash <kavrelibikash@gmail.com>
2025-10-21 21:54:11 +1100

    feat: add Services component with Title integration and update dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import TrustedBy from './components/TrustedBy'
+import Services from './components/Services'
 
 const App = () => {
 
@@ -11,6 +12,7 @@ const App = () => {
        <Navbar theme={theme} setTheme={setTheme} />
        <Hero/>
        <TrustedBy/>
+       <Services />
 
     </div>
   )
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -4,6 +4,7 @@ const Title = ({ title, desc}) => {
   return (
     <>
         <h2 className='text-3xl sm:text-5xl font-medium'>{title}</h2>
+        <p className='max-w-lg text-center text-shadow-red-500'>{desc}</p>
     </>
   )
 }

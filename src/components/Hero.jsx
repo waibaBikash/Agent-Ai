553a1fb25cbc5770899f1f4ee80eceb4a6f6109d
waibaBikash <kavrelibikash@gmail.com>
2025-12-29 21:03:21 +1100

    fix: improve formatting and add missing semicolons in Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,18 +1,24 @@
-import React from 'react'
-import assets from '../assets/assets'
+
+import assets from '../assets/assets';
 
 const Hero = () => {
   return (
-    <div id='hero' className='flex flex-col items-center gap-6 py-20 px-4 sm:px-12 lg:px-24 xl:px-40 text-center w-full overflow-hidden text-gray-700 dark:text-white '>
+    <div id='hero'
+     className='flex flex-col items-center gap-6 py-20 px-4 sm:px-12 lg:px-24 xl:px-40 text-center w-full overflow-hidden text-gray-700 dark:text-white '>
        
-        <div className='inline-flex items-center gap-2 border border-gray-300 p-1.5 pr-4 rounded-full'>
+        <div 
+         className='inline-flex items-center gap-2 border border-gray-300 p-1.5 pr-4 rounded-full'>
            <img src={assets.group_profile} alt="" />
            <p className='text-xs font-medium'>Truested by 10k people</p>
         </div>
 
-        <h1 className='text-4xl sm:text-5xl md:text-6xl xl:text-[84px] font-medium xl:leading-[95px] max-w-5xl'>Truning imagination ioto <span className='bg-gradient-to-r from-[#5044ES] to-[#4d8cea] bg-clip-text text-transparent'>Digital</span> impact.</h1>
+        <h1 
+         className='text-4xl sm:text-5xl md:text-6xl xl:text-[84px] font-medium xl:leading-[95px] max-w-5xl'>Truning imagination ioto
+           <span className='bg-gradient-to-r from-[#5044ES] to-[#4d8cea] bg-clip-text text-transparent'>Digital</span> impact.</h1>
 
-        <p className='text-sm sm:text-lg font-medium text-gray-500 dark:text-white/75 max-w-4/5 sm:max-w-lg pb-3'>Creadting meaningful connectons and turning big ideas into interactive digital experiences.</p>
+        <p className='text-sm sm:text-lg font-medium text-gray-500 dark:text-white/75 max-w-4/5 sm:max-w-lg pb-3'>
+        Creadting meaningful connectons and turning big ideas into interactive digital experiences.
+        </p>
 
          <div className='relative'>
             <img src={assets.hero_img} alt="" className='w-full max-w-6xl' />
@@ -22,4 +28,4 @@ const Hero = () => {
   )
 }
 
-export default Hero
+export default Hero;
